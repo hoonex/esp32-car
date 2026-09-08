9a427695014f88e62835d6f4153929a8dde8e4c7
hoonex <163626004+hoonex@users.noreply.github.com>
2026-09-09 01:09:54 +0900

    Version clean rebuild as Android v4.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "io.github.hoonex.esp32car"
         minSdk = 24
         targetSdk = 36
-        versionCode = 22
-        versionName = "3.3.3"
+        versionCode = 23
+        versionName = "4.0.0"
 
         ndk {
             abiFilters += "arm64-v8a"
