6180faea103ce113945619390ab4ad5f9f823132
hoonex <163626004+hoonex@users.noreply.github.com>
2026-09-12 00:08:56 +0900

    Bump Android app to v4.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "io.github.hoonex.esp32car"
         minSdk = 24
         targetSdk = 36
-        versionCode = 31
-        versionName = "4.0.7"
+        versionCode = 32
+        versionName = "4.0.8"
 
         ndk {
             abiFilters += "arm64-v8a"
